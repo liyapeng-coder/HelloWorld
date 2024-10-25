e65919c53de747baf2c8e91e26f01f3e2aabaf12
liyapeng <liyapeng0303@126.com>
2024-10-25 20:04:26 +0800

    优化冒泡排序算法

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 void sort(int arr[], int size)
 {
     int tmp = 0;
+    bool flag = false;
     for(int i = 0; i < size-1; ++i)
     {
         for(int j = 0; j < size-1-i; ++j)
@@ -13,8 +14,10 @@ void sort(int arr[], int size)
                 tmp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = tmp;
+                flag = true;
             }
         }
+        if(!flag) break;
     }
 }
 int main()
